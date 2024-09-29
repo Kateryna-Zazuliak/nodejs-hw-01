6794b283deaa03ab8fc43e0155aa22eb8c761ca9
Kateryna-Zazuliak <kateryna_didenko@ukr.net>
2024-09-29 17:10:16 +0300

    add tasks

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+    for (let index = 0; index < number; index += 1) {
+      contacts.push(createFakeContact());
+    }
+    await writeContacts(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 generateContacts(5);
